2c4e48295fdd8e4fb22d3f7809165c3100b54a26
Tiago Silva <tiagoalexandresilva7@gmail.com>
2023-12-08 23:17:12 +0000

    tweak navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import ToggleHamburguerMenu from "./components/ToggleHamburguerMenu"
+import ToggleMobileMenu from "./components/ToggleMobileMenu"
 
 function App() {
   return (
@@ -6,17 +6,22 @@ function App() {
       <header>
 
         <div className="container wrapper">
+         
+            <div className="logoTitleContainer__logo">
+              <a href="/" className="title__link">
+                <img src="/logo.svg" alt="logo" />
+              </a>
+            </div>
 
-          <div className="title">
-            <a href="/" className="title__link">
-              <img src="/logo.svg" alt="logo" />
-              <h1>Travelling <em>Projectified</em></h1>
-            </a>
-          </div>
+            <div className="logoTitleContainer__title">
+              <a href="/" className="title__link">
+                <h1>Travelling <em>Projectified</em></h1>
+              </a>
+            </div>
 
-          <nav>
+          <div className="nav">
 
-            <div className="nav">
+            <nav>
 
               <ul className="nav__links">
                 <li>
@@ -30,29 +35,30 @@ function App() {
                 </li>
               </ul>
 
-            </div>
+            </nav>
 
-          </nav>
+          </div>
+
 
           <div className="nav__mobile">
-            <button onClick={ToggleHamburguerMenu} className="nav__mobile--btn"><i className="burguerIcon fa-solid fa-bars"></i></button>
+            <button onClick={ToggleMobileMenu} className="nav__mobile--btn"><i className="burguerIcon fa-solid fa-bars"></i></button>
           </div>
 
         </div>
 
-          <div>
-            <ul className="nav__mobile--links">
-              <li>
-                <a href="/"><i className="fa-solid fa-house-flag"></i> Home</a>
-              </li>
-              <li>
-                <a href="/addlocation"><i className="fa-solid fa-map-location-dot"></i> Travel to...</a>
-              </li>
-              <li>
-                <a href="/about"><i className="fa-solid fa-user-astronaut"></i> About the Tourist</a>
-              </li>
-            </ul>
-          </div>
+        <div>
+          <ul className="nav__mobile--links">
+            <li>
+              <a href="/"><i className="fa-solid fa-house-flag"></i> Home</a>
+            </li>
+            <li>
+              <a href="/addlocation"><i className="fa-solid fa-map-location-dot"></i> Travel to...</a>
+            </li>
+            <li>
+              <a href="/about"><i className="fa-solid fa-user-astronaut"></i> About the Tourist</a>
+            </li>
+          </ul>
+        </div>
 
       </header>
     </>
