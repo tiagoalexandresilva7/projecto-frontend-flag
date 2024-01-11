02e568cc6ee2fefe7530ee2b18d58b18ef8e1565
Tiago Silva <tiagoalexandresilva7@gmail.com>
2024-01-11 13:35:39 +0000

    add new footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,32 @@
-import { Link } from "wouter";
-
 function Footer() {
   return (
     <>
-      <footer className="p-2 flex min-w-full items-center shadow-inner">
-        <div className="container mx-auto">
-          <nav className="whitespace-nowrap text-xl text-blue-600 md:text-2xl">
-            <ul className="flex justify-center gap-4">
-              <li className="hover:scale-105 hover:transition-all hover:duration-300">
-                <Link className="p-4" href="/copyright">
-                  <i className="fa-solid fa-copyright"></i> Copyright
-                </Link>
-              </li>
-              <li className="hover:scale-105 hover:transition-all hover:duration-300">
-                <Link className="p-4" href="/contacts">
-                  <i className="fa-solid fa-square-phone"></i> Contacts
-                </Link>
-              </li>
-            </ul>
-          </nav>
+      <hr className="mx-auto my-8 h-1 w-48 rounded border-0 bg-blue-100 md:my-10" />
+      <footer className="pb-8 text-center">
+        <div className="pb-8">
+          <ul className="flex place-content-center gap-4 text-3xl text-blue-600">
+            <li className="cursor-pointer transition-all hover:scale-125">
+              <a href="mailto:email@example.com">
+                <i className="fa-regular fa-envelope drop-shadow"></i>
+              </a>
+            </li>
+            <li className="cursor-pointer transition-all hover:scale-125">
+              <a href="https://www.linkedin.com/in/tiagoalexandresilva7/">
+                <i className="fa-brands fa-linkedin drop-shadow"></i>
+              </a>
+            </li>
+            <li className="cursor-pointer transition-all hover:scale-125">
+              <a href="https://github.com/tiagoalexandresilva7">
+                <i className="fa-brands fa-github drop-shadow"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="text-lg text-blue-600">
+          <i className="fa-regular fa-copyright pb-2 drop-shadow"></i> Tiago
+          Silva {new Date().getFullYear()}
+          <p>All information and images are illustrative</p>
+          <p>Credits go to their respective owners</p>
         </div>
       </footer>
     </>
