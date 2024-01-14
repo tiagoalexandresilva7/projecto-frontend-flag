499b5bf5773d1fa5deaf1c62979aad5490ade014
Tiago Silva <tiagoalexandresilva7@gmail.com>
2024-01-14 22:39:32 +0000

    change apikey into .env

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,25 +1,31 @@
 import weatherForecast from "./weatherForecast";
 
 let fetchedCoordinates = {};
-let fetchedCityData = {};
-const apikey = "appid=e52c092aae1af777ecf96980c2fd4b06";
 
 async function FetchCityLatLon(city) {
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&${apikey}`;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${
+    import.meta.env.VITE_OPENWEATHER_KEY
+  }`;
 
   const response = await fetch(url);
   const result = await response.json();
 
   const fetchedData = {
+    name: result[0].name,
     lat: result[0].lat,
     lon: result[0].lon,
   };
 
   fetchedCoordinates = fetchedData;
+  return fetchedData;
 }
 
 async function FetchCityWeather(fetchedCoordinates) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${fetchedCoordinates.lat}&lon=${fetchedCoordinates.lon}&${apikey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${
+    fetchedCoordinates.lat
+  }&lon=${fetchedCoordinates.lon}&appid=${
+    import.meta.env.VITE_OPENWEATHER_KEY
+  }&units=metric`;
 
   const response = await fetch(url);
   const result = await response.json();
@@ -36,4 +42,4 @@ async function Query(city) {
   return weather;
 }
 
-export default { Query };
+export default { Query, FetchCityLatLon };
